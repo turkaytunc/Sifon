85699a29d06e208406541d3aa3b2e50c09251ee5
Turkay Tunc <turkaytunc00@gmail.com>
2019-04-28 18:21:44 +0300

    added separate class for save-load functions

---
+++ Sifon/Assets/Scripts/SaveLoadHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System;
+using UnityEngine;
+
+public static  class SaveLoadHandler
+{
+
+
+
+    public static void SaveGame(string saveString)
+    {
+        File.WriteAllText(Application.dataPath + "/save.json", saveString);
+
+    }
+
+    public static string LoadGame()
+    {
+
+        string loadedString = File.ReadAllText(Application.dataPath + "/save.json");
+        return loadedString;
+
+    }
+
+}
