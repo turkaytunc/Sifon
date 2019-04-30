8ae53fb54f780f06891417ff3062a08a160a042d
Turkay Tunc <turkaytunc00@gmail.com>
2019-04-30 20:24:42 +0300

    Added AudioController

---
+++ Sifon/Assets/Scripts/AudioController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioController : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public void SetAudioLevel(float amount)
+    {
+        mixer.SetFloat("MasterAudioVolume", Mathf.Log10(amount) * 20);
+    }
+
+  
+}
