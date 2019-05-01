7fbc5cc86c559ef51ef4641cc2470f4509241c17
Turkay Tunc <turkaytunc00@gmail.com>
2019-05-01 12:19:55 +0300

    Added AnimationControl and Updated PlayerInputs

---
+++ Sifon/Assets/Scripts/AnimationControl.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class AnimationControl : MonoBehaviour
+{
+    private PlayerMovementControl playerMovement;
+    private PlayerInput playerInput;
+    private GameObject firePoint;
+    private Vector2 directionalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMovement = GameObject.Find("Player").GetComponent<PlayerMovementControl>();
+        playerInput = GameObject.Find("Player").GetComponent<PlayerInput>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        directionalInput = playerInput.DirectionalInput;
+        Flip(directionalInput);
+    }
+
+    //Klavye girisine gore karakterin baktigi yonun degistirilmesi
+    private void Flip(Vector2 directionalInput)
+    {
+        firePoint = playerMovement.transform.Find("FirePoint").gameObject;
+
+        if (directionalInput.x > 0)
+        {
+            playerMovement.GetComponent<SpriteRenderer>().flipX = false;
+            firePoint.transform.rotation = Quaternion.Euler(0, 0, 0);
+            firePoint.transform.position = new Vector3(playerMovement.transform.position.x + .25f, playerMovement.transform.position.y, playerMovement.transform.position.z);
+
+
+        }
+        if (directionalInput.x < 0)
+        {
+            playerMovement.GetComponent<SpriteRenderer>().flipX = true;
+            firePoint.transform.rotation = Quaternion.Euler(0, -180, 0);
+            firePoint.transform.position = new Vector3(playerMovement.transform.position.x - .25f, playerMovement.transform.position.y, playerMovement.transform.position.z);
+        }
+    }
+}
--- Sifon/Assets/Scripts/PlayerScripts/PlayerInput.cs
+++ Sifon/Assets/Scripts/PlayerScripts/PlayerInput.cs
@@ -5,32 +5,14 @@ public class PlayerInput : MonoBehaviour
 {
 
     public Vector2 DirectionalInput { get; set; }
-    private PlayerMovementControl player;
 
     private void Start()
     {
-        player = GetComponent<PlayerMovementControl>();
     }
 
     private void Update()
     { 
         DirectionalInput = new Vector2 (Input.GetAxisRaw("Horizontal"), 0);
-        JumpButtonDown();
-
-        Flip(DirectionalInput);
-    }
-
-    //Klavye girisine gore karakterin baktigi yonun degistirilmesi
-    private void Flip(Vector2 directionalInput)
-    {
-        if(directionalInput.x > 0)
-        {
-            player.GetComponent<SpriteRenderer>().flipX = false;
-        }
-        if(directionalInput.x < 0)
-        {
-            player.GetComponent<SpriteRenderer>().flipX = true;
-        }
     }
 
     public bool EscapeButtonDown()
